e2f9c8755ee7cb59abe3b9721754cf8e234aeb9b
AnishShrestha <anishnitro5@gmail.com>
2025-11-21 13:06:53 +0545

    added ObjectMapper bean

--- src/main/java/com/RedisCloudEx/RedisCloudExampleApplication.java
+++ src/main/java/com/RedisCloudEx/RedisCloudExampleApplication.java
@@ -1,7 +1,9 @@
 package com.RedisCloudEx;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisCloudExampleApplication {
@@ -10,4 +12,9 @@ public class RedisCloudExampleApplication {
 		SpringApplication.run(RedisCloudExampleApplication.class, args);
 	}
 
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
 }
