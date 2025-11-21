72261ee9c6d69cbd5d4070a64e7d53d3bed60447
AnishShrestha <anishnitro5@gmail.com>
2025-11-21 11:24:12 +0545

    Getting Started

---
+++ src/main/java/com/RedisCloudEx/RedisCloudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.RedisCloudEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCloudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCloudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RedisCloudEx/RedisCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RedisCloudEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCloudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
